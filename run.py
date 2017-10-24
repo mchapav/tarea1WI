8533a4599b54723ebfd06e5984f1cb35494c5eb0
Mario Schiappacasse <mario.schiappacasse@ing.uchile.cl>
2017-10-24 19:25:53 -0300

    Listo lo del plot de la red

--- run.py
+++ run.py
@@ -1,13 +1,34 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from networkx import DiGraph
+import networkx as nx
 import pandas as pd
-
+import matplotlib.pyplot as plt
+from networkx_viewer import Viewer
+import threading
 import csv
+
+
 
 ##########################################
 # Abrir archivos
 ##########################################
 
-relation = pd.read_csv("data/relation5.csv")
+relation = pd.read_csv("data/relation5.csv", sep = ";") #user_id , rel
 
+subrelation = relation.sample(n=1000)
+
+G= nx.DiGraph()
+
+for index, row in subrelation.iterrows():
+    G.add_node(int(row["user_id"]))
+
+for index, row in subrelation.iterrows():
+    if isinstance(row["rel"],str):
+        rels = row["rel"].split(" ")
+        for x in rels:
+            x = int(x)
+            if(G.has_node(x)):
+                G.add_edge(row["user_id"], x)
+
+nx.draw(G)
+plt.show()
