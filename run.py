38819e84943ccaf7e6edc2d5dcacf1738f199483
Mario Schiappacasse <mario.schiappacasse@ing.uchile.cl>
2017-10-24 16:46:35 -0300

    Proyecto vacio

---
+++ run.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from networkx import DiGraph
+import pandas as pd
+
+import csv
+
+##########################################
+# Abrir archivos
+##########################################
+
+try:
+    relations = pd.read.csv("data/relations5.csv")
+
+
+
+
+
+
+##########################################
+#  Creación de Grafo
+##########################################
+#G = DiGraph()
+#i=1
+#with open("relation5.csv") as file:
+ #   reader = pd.read.csv(file)
+
+
